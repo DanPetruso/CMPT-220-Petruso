0a70bd244a332706ada6ec53a603ad67aa682ebd
Dan Petruso <daniel.petruso1@marist.edu>
2018-02-09 16:31:09 -0500

    small change to 6.1

--- labs/3/lab3_problem6_1.java
+++ labs/3/lab3_problem6_1.java
@@ -7,7 +7,7 @@ public class lab3_problem6_1 {
 	
 	public static void main(String[] args) {
 		for(int i = 1; i <= 100; i++) {
-			System.out.print(getPentagonalNumber(i));
+			System.out.printf("%7d", getPentagonalNumber(i));
 			System.out.print(i % 10 == 0? "\n": " ");
 		}
 
