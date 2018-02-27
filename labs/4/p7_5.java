da4f436394c5da65bf45edda6c136ec143a3cc38
Juan Arias <juan.arias@marist.edu>
2018-02-26 23:06:07 -0500

    Feedback

--- labs/4/p7_5.java
+++ labs/4/p7_5.java
@@ -14,14 +14,14 @@ public class p7_5 {
 		for(int i = 0; i < 10; i++) {
 			boolean isDistinct = true;
 			
-			for(int j = 0; j < 10; j++) {
+			for(int j = 1; j < 10; j++) { // JA: If you start at 0, the condition will always be true
 				if(arr[i] == arr[j]) {
 					isDistinct = false;
 					break;
 				}
 			}
 			
-			if(isDistinct)
+			if(isDistinct) // JA: You also need to add one instance of the number (even when duplicate)
 				list.add(arr[i]);
 		}
 		System.out.println("The number of distinct numbers is " + list.size());
